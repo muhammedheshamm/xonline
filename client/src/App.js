818ddd1cc5c4ade11db0bcc39f83286b24792c56
Muhammed Hesham <muhammedheshamm1@gmail.com>
2023-06-12 22:00:39 +0300

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,6 @@ import Entry from './components/Entry/Entry';
 
 function App() {
 
-  const documentHeight = () => {
-    const body = document.querySelector('body')
-    const html = document.querySelector('html')
-    const app = document.querySelector('.App')
-    body.style.setProperty('--doc-height', `${window.innerHeight}px`)
-    html.style.setProperty('--doc-height', `${window.innerHeight}px`)
-    app.style.setProperty('--doc-height', `${window.innerHeight}px`)
-  }
-  window.addEventListener('resize', documentHeight)
-  documentHeight()
-
   return (
     <div className="App">
       <Entry />
