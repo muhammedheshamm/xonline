5c94b6620691be5f0a4a31b673b9abfacfe136ba
Muhammed Hesham <muhammedheshamm1@gmail.com>
2023-06-12 21:48:24 +0300

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,12 @@ import Entry from './components/Entry/Entry';
 function App() {
 
   const documentHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+    const body = document.querySelector('body')
+    const html = document.querySelector('html')
+    const app = document.querySelector('.App')
+    body.style.setProperty('--doc-height', `${window.innerHeight}px`)
+    html.style.setProperty('--doc-height', `${window.innerHeight}px`)
+    app.style.setProperty('--doc-height', `${window.innerHeight}px`)
   }
   window.addEventListener('resize', documentHeight)
   documentHeight()
