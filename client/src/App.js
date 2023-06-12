998a4430c19e95e1a5cbb7245226a5d202b0fa70
Muhammed Hesham <muhammedheshamm1@gmail.com>
2023-06-12 18:45:13 +0300

    fixed body height

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import Entry from './components/Entry/Entry';
 
 function App() {
+
+  const documentHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+  }
+  window.addEventListener('resize', documentHeight)
+  documentHeight()
+
   return (
     <div className="App">
       <Entry />
